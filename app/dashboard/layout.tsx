1362166d7cd36dff15019c74c29f7ae3ce98fec5
Olaniyi John <olaniyijohnayomide@gmail.com>
2025-07-08 15:12:47 +0100

    automatic log out after 5 minutes of inactive

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,24 +1,4 @@
-// import { DashboardNav } from "@/components/dashboard/nav"
-// import { Sidebar } from "@/components/dashboard/sidebar"
-
-// export default function DashboardLayout({
-//   children,
-// }: {
-//   children: React.ReactNode
-// }) {
-//   return (
-//     <div className="flex h-screen">
-//       <Sidebar />
-//       <div className="flex-1 flex flex-col">
-//         <DashboardNav />
-//         <main className="flex-1 overflow-y-auto bg-lighter p-8">{children}</main>
-//       </div>
-//     </div>
-//   )
-// }
-
 "use client";
-
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { DashboardNav } from "@/components/dashboard/nav";
