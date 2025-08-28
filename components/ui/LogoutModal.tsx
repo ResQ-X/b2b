57bb62e764524c38d764849be486d61d5b92b12c
Olaniyi John <olaniyijohnayomide@gmail.com>
2025-08-28 19:50:48 +0100

    fix: adjust layout and button styles in LogoutModal component

--- components/ui/LogoutModal.tsx
+++ components/ui/LogoutModal.tsx
@@ -18,25 +18,25 @@ export default function LogoutModal({
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/60">
-      <div className="w-[547px] rounded-2xl bg-[#3B3835] p-6 text-center text-[#fff] shadow-lg">
+      <div className="w-11/12 lg:w-[547px] rounded-2xl bg-[#3B3835] p-6 text-center text-[#fff] shadow-lg">
         <h2 className="text-2xl font-semibold">Log Out?</h2>
         <p className="my-9 text-lg font-medium">
           Are you sure you want to log out? You’ll need to log in again to
           access your account.
         </p>
 
-        <div className="my-8 flex justify-between gap-4">
+        <div className="my-8 flex flex-col lg:flex-row justify-between gap-4">
           <Button
             variant="grey"
             onClick={onClose}
-            className="w-[224px] h-[48px] lg:h-[52px]"
+            className="w-full lg:w-[224px] h-[48px] lg:h-[52px]"
           >
             Cancel
           </Button>
           <Button
             variant="orange"
             onClick={onConfirm}
-            className="w-[224px] h-[48px] lg:h-[52px]"
+            className="w-full lg:w-[224px] h-[48px] lg:h-[52px]"
           >
             Logout
           </Button>
