52bab87b7aa530c74ce3ad933fe31245d5b9d284
Olaniyi John <olaniyijohnayomide@gmail.com>
2026-01-09 12:20:19 +0100

    added currency formatter functionality

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,9 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const formatCurrency = (n: string) =>
+  `₦${Number(n).toLocaleString(undefined, { maximumFractionDigits: 0 })}`;
