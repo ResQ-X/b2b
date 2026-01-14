cbe54b5fc7004c8c7f57b1345fe5532edd13fd36
Olaniyi John <olaniyijohnayomide@gmail.com>
2026-01-14 14:50:17 +0100

    hide manual top up for sub accounts

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,3 +7,22 @@ export function cn(...inputs: ClassValue[]) {
 
 export const formatCurrency = (n: string) =>
   `₦${Number(n).toLocaleString(undefined, { maximumFractionDigits: 0 })}`;
+
+type UserRole = "USER" | "SUPER" | "SUB";
+
+type UserRoleEnum = {
+  USER: "USER";
+  SUPER: "SUPER";
+  SUB: "SUB";
+};
+
+const UserRoleEnum: UserRoleEnum = {
+  USER: "USER",
+  SUPER: "SUPER",
+  SUB: "SUB",
+};
+export const ROLE_PERMISSIONS: Record<UserRole, { canManualTopUp: boolean }> = {
+  USER: { canManualTopUp: true },
+  SUPER: { canManualTopUp: true },
+  SUB: { canManualTopUp: false },
+};
